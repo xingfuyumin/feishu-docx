3145e71a89cbca2de94bc22dcadf51d218c9c188
杨超 <chao.yang@thinkingdata.cn>
2023-12-19 16:37:36 +0800

    fix: update

--- src/FeishuDocxRender/components/image/index.tsx
+++ src/FeishuDocxRender/components/image/index.tsx
@@ -12,11 +12,14 @@ type Props = {
 export default memo((({
   data, render,
 }) => {
+  const containerWidth = document.querySelector('.feishudocx-container')?.clientWidth;
+  const width = containerWidth ? (containerWidth - 64 ): data?.image.width || 0;
+  const height = width / (data?.image.width || 0) * (data?.image.height || 0);
   const tsx = data ? (
     <div
       key={data.block_id}
       id={data.block_id}
-      style={{ width: data.image.width }}
+      style={{ width: '100%', minHeight: height }}
       className={classNames(
         'feishudocx-image',
         data.image?.align ? `feishudocx-textstyle-align-${data.image?.align}` : '',
